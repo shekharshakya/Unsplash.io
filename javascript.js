c766d9d71eb4bb23c0ffea2c00f75b4956b3a2ec
shekhar <shekhar8979533238@gmail.com>
2021-11-04 12:48:07 +0530

    sec commit

--- javascript.js
+++ javascript.js
@@ -1,21 +1,44 @@
+//after window is loaded completely 
+window.onload = function() {
+    //hide the preloader
+    document.querySelector(".wrapper").style.display = "none";
+}
+
+//contact form validation
+function validation() {
+    var user = document.getElementById("username").value;
+    var gmail = document.getElementById("email").value;
+    if (user == "") {
+        alert("please fill the username field");
+        user.classList.add("alertcss");
+        return false;
+    }
+    if ((user.length <= 2) || (user.length > 20)) {
+        alert("please fill the username length  3 t0 19");
+        return false;
+    }
+
+    if (gmail == "") {
+        alert("please fill the email field");
+        return false;
+    }
+    if (gmail.indexOf("@") <= 0) {
+        alert("please fill the email field");
+        return false;
+    }
+    if ((gmail.charAt(email.length - 4) != ".") && (gmail.charAt(email.length - 2) != ".")) {
+        // alert("Invalid Email ID");
+        return false;
+    }
+}
+
+//contact form validation
+
 // open menu function
 function openmenu() {
     document.getElementById("navlists").classList.toggle("open-menu");
 }
 // open menu function
-
-//modal box 
-//modal box timing function come after timer
-setTimeout(function() {
-    document.getElementById("cookies-container").classList.add("cookiesContentOpen");
-}, 3000);
-//modal box timing function come after timer
-// close menu function
-function closemodal() {
-    document.getElementById("cookies-container").classList.add("cookiesContentClose");
-}
-// close menu function
-//modal box 
 
 // scroll top button in right side of the screen start
 var mybutton = document.getElementById("myBtn"); //Get the button
@@ -36,11 +59,58 @@ function topFunction() {
 }
 // scroll top button in right side of the screen  end 
 
+// javascript line chart
+window.onload = function() {
 
+        var chart = new CanvasJS.Chart("chartContainer", {
+            animationEnabled: true,
+            theme: "light2",
+            title: {
+                text: "Line Chart"
+            },
+            data: [{
+                type: "line",
+                indexLabelFontSize: 16,
+                dataPoints: [
+                    { y: 460 },
+                    { y: 400 },
+                    { y: 500, indexLabel: "\u2191 highest", markerColor: "red", markerType: "triangle" },
+                    { y: 460 },
+                    { y: 450 },
+                    { y: 500 },
+                    { y: 480 },
+                    { y: 480 },
+                    { y: 410, indexLabel: "\u2193 lowest", markerColor: "DarkSlateGrey", markerType: "cross" },
+                    { y: 500 },
+                    { y: 480 },
+                    { y: 510 }
+                ]
+            }]
+        });
+        chart.render();
+    }
+    // javascript line chart
 
 
 //jquery code start here
 $(document).ready(function() {
+
+    // automatic popUp modal
+    setTimeout(function() {
+            $('.automatic-popUp').css(
+                'opacity', '1');
+            console.log("hello shekhar");
+        }, 3000)
+        //close automatic popUp
+    $('.close').click(function() {
+        $('.automatic-popUp').css('display', 'none');
+    });
+    //close automatic popUp
+    $('.accept').click(function() {
+        $('.automatic-popUp').css('display', 'none');
+    });
+    // automatic popUp modal
+
     // testimonials slick slider start
     $('.slider').slick({
         dots: true,
@@ -76,19 +146,26 @@ $(document).ready(function() {
                 }
             }
         ]
-    }); // testimonials slick slider end
+    });
+    // testimonials slick slider end
     // start waypoint start
+    $("#about").waypoint(function(direction) {
+        $(".about-right-img").addClass("animate__animated animate__fadeInUp")
+        $(".about-details").addClass("animate__animated animate__fadeInLeft")
+        $(".about-bg").addClass("animate__animated animate__fadeInRight")
+    }, {
+        offset: "40%"
+    });
+    $(".services").waypoint(function(direction) {
+        $(".services-item").addClass("animate__animated animate__zoomIn")
 
-    // $("").waypoint(function(direction) {
-    //     $("").addClass("")
-    // })
+    }, {
+        offset: "40%"
+    });
+    $("#chartContainer").waypoint(function(direction) {
+        $("#chartContainer").addClass("animate__animated animate__fadeInRight")
+
+    }, {
+        offset: "40%"
+    });
 })
-
-// // way point
-// var waypoint = new Waypoint({
-//     element: document.getElementById('px-offset-waypoint'),
-//     handler: function(direction) {
-//         notify('I am 20px from the top of the window')
-//     },
-//     offset: 20
-// })
